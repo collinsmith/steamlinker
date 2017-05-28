d5294709a9439aad7782a93ededbddfc0c81f0c4
Collin Smith <collinsmith70@gmail.com>
2017-05-28 03:18:22 -0700

    Transitioned to a better icons scheme which supports better material design Added support for multiple repositories Added support for Windows junction detection (may be janky as it uses JNI)

---
+++ src/com/gmail/collinsmith70/steamlinker/JunctionSupport.java
@@ -0,0 +1,47 @@
+package com.gmail.collinsmith70.steamlinker;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.WString;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class JunctionSupport {
+
+  private JunctionSupport() {
+  }
+
+  interface Kernel32 extends Library {
+    int GetFileAttributesW(WString fileName);
+  }
+
+  static Kernel32 lib = null;
+
+  public static int getWin32FileAttributes(@NotNull Path path) throws IOException {
+    if (lib == null) {
+      synchronized (Kernel32.class) {
+        lib = (Kernel32) Native.loadLibrary("kernel32", Kernel32.class);
+      }
+    }
+
+    return lib.GetFileAttributesW(new WString(path.toAbsolutePath().toString()));
+  }
+
+  public static boolean isJunctionOrSymlink(@NotNull Path path) throws IOException {
+    if (!Files.exists(path)) {
+      return false;
+    }
+
+    int attributes = getWin32FileAttributes(path);
+    if (-1 == attributes) {
+      return false;
+    }
+
+    return ((0x400 & attributes) != 0);
+  }
+
+}
--- src/com/gmail/collinsmith70/steamlinker/Main.java
+++ src/com/gmail/collinsmith70/steamlinker/Main.java
@@ -1,27 +1,39 @@
 package com.gmail.collinsmith70.steamlinker;
 
 import org.jetbrains.annotations.NotNull;
+import org.kordamp.ikonli.javafx.FontIcon;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.prefs.Preferences;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Control;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.DialogPane;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MultipleSelectionModel;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputControl;
 import javafx.scene.image.Image;
@@ -31,6 +43,8 @@ import javafx.stage.Stage;
 import javafx.util.Pair;
 
 public class Main extends Application {
+
+  private static final boolean DEBUG_REPOSITORIES_AUTOCONFIG = true;
 
   private static final Preferences PREFERENCES = Preferences.userNodeForPackage(Main.class);
 
@@ -44,42 +58,34 @@ public class Main extends Application {
     Scene scene = new Scene(root);
 
     stage.setTitle(Bundle.translate("name"));
-    doIcons(stage);
+    stage.getIcons().add(new Image("mipmap/icon_16x16.png"));
+    stage.getIcons().add(new Image("mipmap/icon_32x32.png"));
     stage.setScene(scene);
     stage.show();
 
     configureSteamDir(scene);
   }
 
-  private void doIcons(@NotNull Stage stage) {
-    stage.getIcons().add(new Image("mipmap/icon_16x16.png"));
-    stage.getIcons().add(new Image("mipmap/icon_32x32.png"));
-  }
-
   private void configureSteamDir(@NotNull Scene scene) {
-    TextField steamDir = (TextField) scene.lookup("#commonDir");
+    TextInputControl steamDir = (TextInputControl) scene.lookup("#commonDir");
     steamDir.setText(PREFERENCES.get("common.dir", ""));
   }
 
-  @FXML
-  private void onChangeSteamDir(@NotNull ActionEvent event) {
-    Node source = (Node) event.getSource();
-    Scene scene = source.getScene();
-    TextInputControl initialDirectory = (TextInputControl) scene.lookup("#commonDir");
-    String text = initialDirectory.getText();
-    Path path = Paths.get(text);
+  private void configureRepositories(@NotNull Scene scene) {
+    String value = PREFERENCES.get("repos", "");
+    List<String> paths = Arrays.asList(value.split(";"));
+    ObservableList<String> items = FXCollections.observableList(paths);
 
-    DirectoryChooser directoryChooser = new DirectoryChooser();
-    if (!text.isEmpty() && Files.isDirectory(path)) {
-      directoryChooser.setInitialDirectory(path.toFile());
-    }
+    ListView repos = (ListView) scene.lookup("#repos");
+    repos.setItems(items);
 
-    File result = directoryChooser.showDialog(scene.getWindow());
-    if (result != null) {
-      text = result.getAbsolutePath();
-      initialDirectory.setText(text);
-      //PREFERENCES.put("common.dir", text);
-    }
+    MultipleSelectionModel selectionModel = repos.getSelectionModel();
+    selectionModel.clearSelection();
+    selectionModel.setSelectionMode(SelectionMode.SINGLE);
+    selectionModel.selectedIndexProperty().addListener((observable, oldValue, newValue) -> {
+      Button removeRepository = (Button) scene.lookup("#removeRepository");
+      removeRepository.setDisable((int) newValue == -1);
+    });
   }
 
   @FXML
@@ -90,7 +96,7 @@ public class Main extends Application {
     Scene thisScene = control.getScene();
 
     Stage stage = (Stage) dialog.getDialogPane().getScene().getWindow();
-    stage.getIcons().add(new Image("png/settings-4.png"));
+    stage.getIcons().add(new FontIcon("gmi-settings:32:grey").snapshot(null, null));
     stage.setOnCloseRequest(closeEvent -> dialog.close());
 
     DialogPane content = FXMLLoader.load(Main.class.getResource("/layout/settings.fxml"), Bundle.BUNDLE);
@@ -101,6 +107,7 @@ public class Main extends Application {
     dialog.setResizable(true);
     Scene scene = content.getScene();
     configureSteamDir(scene);
+    configureRepositories(scene);
     dialog.setResultConverter(buttonType -> {
       if (buttonType != ButtonType.APPLY) {
         return null;
@@ -110,6 +117,10 @@ public class Main extends Application {
 
       Map<String, Object> result = new HashMap<>();
       result.put("#commonDir", initialDirectory.getText());
+
+      ListView repos = (ListView) scene.lookup("#repos");
+      ObservableList items = repos.getItems();
+      result.put("#repos", String.join(";", items));
 
       return new Pair<>(buttonType, result);
     });
@@ -124,5 +135,99 @@ public class Main extends Application {
 
     TextField steamDir = (TextField) thisScene.lookup("#commonDir");
     steamDir.setText(initialDirectory);
+
+    String repos = (String) map.get("#repos");
+    PREFERENCES.put("repos", repos);
+  }
+
+  @FXML
+  private void onChangeSteamDir(@NotNull ActionEvent event) {
+    Scene scene = ((Node) event.getSource()).getScene();
+    TextInputControl initialDirectory = (TextInputControl) scene.lookup("#commonDir");
+    String text = initialDirectory.getText();
+    Path path = Paths.get(text);
+
+    File existingFile = !text.isEmpty() && Files.isDirectory(path)
+        ? path.toFile()
+        : null;
+
+    DirectoryChooser directoryChooser = new DirectoryChooser();
+    if (existingFile != null) {
+      directoryChooser.setInitialDirectory(existingFile);
+    }
+
+    File steamDir = directoryChooser.showDialog(scene.getWindow());
+    if (steamDir != null) {
+      if (existingFile != null && existingFile.equals(steamDir) && !DEBUG_REPOSITORIES_AUTOCONFIG) {
+        return;
+      }
+
+      String steamDirPath = steamDir.getAbsolutePath();
+      initialDirectory.setText(steamDirPath);
+
+      Alert autoconfig = new Alert(Alert.AlertType.CONFIRMATION,
+          Bundle.translate("autoconfig.message"),
+          ButtonType.YES, ButtonType.NO);
+      autoconfig.setTitle(Bundle.translate("autoconfig.title"));
+      autoconfig.setHeaderText(null);
+
+      Stage stage = (Stage) autoconfig.getDialogPane().getScene().getWindow();
+      stage.getIcons().add(new FontIcon("gmi-info:16:red").snapshot(null, null));
+      stage.setOnCloseRequest(closeEvent -> autoconfig.close());
+
+      autoconfig.showAndWait()
+          .filter(result -> result == ButtonType.YES)
+          .ifPresent(result -> autoconfigRepositories(scene, steamDirPath));
+    }
+  }
+
+  private void autoconfigRepositories(@NotNull Scene scene, @NotNull String steamDirPath) {
+    Path path = Paths.get(steamDirPath);
+    Set<String> uniqueRepositories = new HashSet<>();
+    try {
+      Files.list(path)
+          .filter(p -> {
+            try {
+              return JunctionSupport.isJunctionOrSymlink(p);
+            } catch (IOException e) {
+              e.printStackTrace();
+              return false;
+            }
+          })
+          .forEach(symlink -> {
+            try {
+              uniqueRepositories.add(symlink.toRealPath().getParent().toString());
+            } catch (IOException e) {
+              e.printStackTrace();
+            }
+          });
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    ObservableList<String> items = FXCollections.observableArrayList(uniqueRepositories);
+    ListView repos = (ListView) scene.lookup("#repos");
+    repos.setItems(items);
+  }
+
+  @FXML
+  private void onAddRepo(@NotNull ActionEvent event) {
+    Scene scene = ((Node) event.getSource()).getScene();
+    DirectoryChooser directoryChooser = new DirectoryChooser();
+    File steamDir = directoryChooser.showDialog(scene.getWindow());
+    if (steamDir != null) {
+      String steamDirPath = steamDir.getAbsolutePath();
+      ListView repos = (ListView) scene.lookup("#repos");
+      repos.getItems().add(steamDirPath);
+    }
+  }
+
+  @FXML
+  private void onRemoveRepo(@NotNull ActionEvent event) {
+    Scene scene = ((Node) event.getSource()).getScene();
+    ListView repos = (ListView) scene.lookup("#repos");
+    MultipleSelectionModel model = repos.getSelectionModel();
+    repos.getItems().remove(model.getSelectedIndex());
+    model.clearSelection();
   }
 }
